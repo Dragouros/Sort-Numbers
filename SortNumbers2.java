7feae57127e98163082c6e66368c4b747f69b539
Dragouros <47721397+Dragouros@users.noreply.github.com>
2019-03-13 20:41:51 -0400

    Add files via upload

---
+++ SortNumbers2.java
@@ -0,0 +1,49 @@
+package com.Luigi;
+
+import java.util.*;
+
+public class SortNumbers2 {
+	
+	SortNumbers2() {
+	}
+
+	public void OrderNumbers(ArrayList<Integer> Numbers) {
+		ArrayList<Integer> OrderList = new ArrayList<Integer>();
+		for(int i = 0; i < Numbers.size(); i++) {
+			for(int j = 0; j < Numbers.size() - i; j++) {
+				if(Numbers.get(j) > Numbers.get(j + 1)) {
+					OrderList.add(Numbers.get(i));
+				}
+			}
+		}
+		System.out.println(OrderList);
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> Count = new ArrayList<Integer>() {{
+		add(36);
+		add(22);
+		add(1);
+		add(37);
+		add(12);
+		add(13);
+		add(29);
+		add(17);
+		add(27);
+		add(10);
+		add(20);
+		add(30);
+		add(7);
+		add(9);
+		add(17);
+		add(31);
+		add(33);
+		add(38);
+		add(23);
+		add(24);
+		add(15);
+		}};
+	    SortNumbers MyAnalyze = new SortNumbers();
+	    MyAnalyze.Sort(Count);
+	}
+}
